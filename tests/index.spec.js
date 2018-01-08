e5570067bd792235941ed3f691bbf4311cb16c77
Tung Vu <me@tungv.com>
2018-01-08 10:14:32 +0700

    test: add test that log error when loading resolvers failed

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -3,7 +3,23 @@ import test from 'ava';
 import path from 'path';
 import { graphql } from 'graphql';
 
+const logs = [];
+let originalLog = console.log;
+
+test.before(() => {
+  console.log = (...args) => {
+    logs.push(args);
+    // uncomment this for debugging
+    // return originalLog.apply(console, args);
+  };
+});
+
+test.after(() => {
+  console.log = originalLog;
+});
+
 test('should load', async t => {
+  logs.length = 0;
   const schema = load(path.resolve(__dirname, '../fixtures/test_root_1'));
   const resp = await graphql(
     schema,
@@ -16,4 +32,30 @@ test('should load', async t => {
   );
 
   t.deepEqual(resp, { data: { sum: 3 } });
+  t.true(logs.length === 0);
+});
+
+test('should log error if resolvers are failed to load', t => {
+  logs.length = 0;
+  const schema = load(
+    path.resolve(__dirname, '../fixtures/test_error_resolver_root')
+  );
+
+  const absPathToErrorFile = path.resolve(
+    __dirname,
+    '../fixtures/test_error_resolver_root/error.resolver.js'
+  );
+
+  t.deepEqual(logs, [
+    [
+      `
+┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+│                                                                                                                 │
+│   Error while loading "${absPathToErrorFile}"   │
+│                                                                                                                 │
+│   Cannot find module 'something'                                                                                │
+│                                                                                                                 │
+└─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘`,
+    ],
+  ]);
 });
